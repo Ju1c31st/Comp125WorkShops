4d2c2dc080ee537dc1f8fca6b917fcc00abf9187
Jonah Skinner <jonah.lee.skinner@gmail.com>
2017-04-13 10:30:47 +1000

    PreParing for the practical exam
    Added the some of the main components and now need to revise selection sort and array algorithms

--- AllInOne.java
+++ AllInOne.java
@@ -33,7 +33,10 @@ class StockItem {
 	 * (validate such that if parameter is 
 	 * negative, instance variable is assigned 0)
 	 */
-	
+	public stockItem(){
+		unitPrice = 0.1;
+		quantity = 1;
+	}
 	/*
 	 * default constructor
 	 * unitPrice should be 0.1
@@ -41,21 +44,34 @@ class StockItem {
 	 */
 	
 	//parameterized constructor
-
+	public stockItem(double unitPrice, int quantity){
+		setUnitPrice(unitPrice);
+		setQuantity(quantity);
+	}
 	/*
 	 * method totalPrice, returns the total 
 	 * price of all stock of the item. so if
 	 * 5 units are left @ $1.5 per unit, total
 	 * stock price is 5 * 1.5 = 7.5 
 	 */
-	
+	public totalPrice(){
+		return unitPrice*quantity;
+	}
 	/* compareTo(StockItem other)
 	 * return:
 	 * 1 if unitPrice of calling object is more than that of parameter object 
 	 * -1 if unitPrice of calling object is less than that of parameter object
 	 *  0 if unitPrice of calling object is equal to that of parameter object
 	 */
-}
+	public int compareTo(other StockItem){
+		if(this.unitPrice > other.unitPrice){
+			return 1;
+		}
+		if(this.unitPrice > other.unitPrice){
+			return -1;
+		}
+		return 0;
+	}
 
 
 public class AllInOne {
@@ -66,7 +82,10 @@ public class AllInOne {
 	 * return 1 if array is null or empty
 	 */
 	public static int product(int[] arr) {
-		return 1; //to be completed
+		int result = 0; 
+		for(int i=0; i<arr.length; i++){
+			arr[i] += result;
+		return result; //to be completed
 	}
 
 	/**
@@ -80,7 +99,21 @@ public class AllInOne {
 	 * return false otherwise (or if arr is null or empty)
 	 */
 	public static boolean allInRange(int[] arr, int low, int high) {
-		return true; //to be completed
+		boolean check = false;
+		if(arr == null){ 
+			return check;
+		}
+		if(arr.length = 0){
+			return check;
+		}
+		for(int i=0; i<arr.length; i++){
+			if(arr[i] > high || arr[i] < low){
+				return check;
+			}
+			check = true;
+		}
+			
+		return check; //to be completed
 	}
 
 	/**
@@ -91,17 +124,19 @@ public class AllInOne {
 	 * return -1 if array is null or target doesn't exist in arr
 	 */
 	public static int binarySearch(int[] arr, int target) {
-		boolean dummy = false;
+		boolean found = false;
 		int first = 0; 
-		int last = 123456789; //replace 123456789 by the correct value
-		while( dummy ) { //replace dummy by the correct expression		
+		int last = arr.length; //replace 123456789 by the correct value
+		while( first<last && !found ) { //replace dummy by the correct expression		
 			int median = (first + last)/2;		
-			if (dummy) //replace dummy by the correct expression
+			if (median == target) //replace dummy by the correct expression
+				found = true;
 				return median;
+			
 			if (target < arr[median])
 				last = median - 1; //search left
 			else
-				first = 123456789; //search right: replace 123456789 by the correct value
+				first = median + 1; //search right: replace 123456789 by the correct value
 		}
 		return -1;
 	}
@@ -115,7 +150,7 @@ public class AllInOne {
 		if(arr == null)
 		       return;
 		    
-		  for(int i=0; dummyExpression; i++) { //replace dummyExpression by correct expression
+		  for(int i=0; arr.length-1; i++) { //replace dummyExpression by correct expression
 		    int minIndex = i; 
 		    for(int k = i + 1; k < arr.length; k++) { 
 		      if (dummyExpression) { //replace dummyExpression by correct expression
@@ -141,6 +176,7 @@ public class AllInOne {
 	 * satisfying the condition
 	 */
 	public static int[] getItemsInRange(int[] arr, int low, int high) {
+		
 		return null; //to be completed
 	}
 }
